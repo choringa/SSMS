898bb162a3654b35409c444409e15821ff0684a8
Choringa <d.arteaga28@gmail.com>
2017-08-23 23:06:03 -0300

    share text activity interfaz inicio logica

--- app/src/main/java/com/indi/ssms/ShareTextActivity.java
+++ app/src/main/java/com/indi/ssms/ShareTextActivity.java
@@ -1,47 +1,96 @@
 package com.indi.ssms;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ShareTextActivity extends AppCompatActivity {
 
 
+    private static final String TAG = "ShareTextActivity";
+
     private Toolbar toolbar;
+    private Button btnShare, btnPreview;
+    private TextView etPlainText, etCryptText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_text);
 
+        etPlainText = findViewById(R.id.etSharePlainText);
+        etCryptText = findViewById(R.id.etShareCryptText);
 
-        /**
-
-        btnShareRedes = view.findViewById(R.id.btnShare);
-        btnShareRedes.setOnClickListener(new View.OnClickListener() {
+        btnShare = findViewById(R.id.btnShare);
+        btnShare.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: share button
                 Log.i(TAG, "btnSharePressed");
-                Intent share = new Intent(Intent.ACTION_SEND);
-                share.setType("text/plain");
-
-                // You can add subject also
-                 //sharingIntent.putExtra( android.content.Intent.EXTRA_SUBJECT,
-                 //"Subject Here");
-
-                share.putExtra(android.content.Intent.EXTRA_TEXT, "el texto cifrado");
-                mainActivity.startActivity(Intent.createChooser(share,"Share via"));
+                shareButton();
             }
         });
-        */
+
+        btnPreview = findViewById(R.id.btnPreview);
+        btnPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.i(TAG, "btnPreview");
+                previewCrypt(view);
+            }
+        });
+
 
         setToolbar();
         if (getSupportActionBar() != null) // Habilitar up button
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    /**
+     * Metodo que da visibilidad al usuario de su texto ya cifrado antes de que lo comparta.
+     */
+    private void previewCrypt(View view) {
+        String textoPlano = etPlainText.getText().toString();
+        if(!textoPlano.trim().equals("")){
+            String encryptedMessage = cryptMessage(textoPlano);
+            etCryptText.setVisibility(View.VISIBLE);
+            etCryptText.setText(encryptedMessage);
+        }
+        else {
+            Snackbar.make(view, "You must insert a message", Snackbar.LENGTH_LONG).setAction("Action", null).show();
+        }
+
+    }
+
+    /**
+     * Metodo que encripta el mensaje
+     * @param textoPlano el texto plano
+     * @return el texto cifrado
+     */
+    private String cryptMessage(String textoPlano) {
+        return textoPlano.toCharArray().toString();
+    }
+
+
+    /**
+     * Metodo que pemrite al usuario compartir el mensaje encriptado por las diferentes aplicaciones que permiten
+     * el ingreso de texto plano y que el usuario tenga instaladas.
+     */
+    private void shareButton() {
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("text/plain");
+
+        // You can add subject also
+        //sharingIntent.putExtra( android.content.Intent.EXTRA_SUBJECT,
+        //"Subject Here");
+
+        share.putExtra(android.content.Intent.EXTRA_TEXT, "el texto cifrado");
+        startActivity(Intent.createChooser(share,"Share via"));
     }
 
     private void setToolbar() {
